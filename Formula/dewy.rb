51ac71d7cc37368b9c97e7240780f46a942a2531
Aly Raffauf <aly@raffauflabs.com>
2026-04-05 22:16:04 -0400

    add dewy

---
+++ Formula/dewy.rb
@@ -0,0 +1,18 @@
+class Dewy < Formula
+  desc "Minimal terminal UI for Todoist"
+  homepage "https://github.com/alyraffauf/dewy"
+  url "https://github.com/alyraffauf/dewy/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "f8c10c75e0ded404c61acb3274ca49c90ddc517b85829ebceb1d1f2026a39cb1"
+  license "GPL-3.0-or-later"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"bin/dewy"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dewy --version")
+  end
+end
--- README.md
+++ README.md
@@ -13,9 +13,16 @@ brew tap alyraffauf/tap
 | Formula | Description |
 |---------|-------------|
 | `atbbs` | AT Protocol bulletin board system |
+| `dewy`  | Minimal terminal UI for Todoist |
 
 ### atbbs
 
 ```
 brew install atbbs
+```
+
+### dewy
+
+```
+brew install dewy
 ```
