383d373e254aac3442f4abddc65a0545325d4272
Aly Raffauf <aly@aly.codes>
2026-07-12 14:01:51 -0400

    obsidian-headless: update python dependency to 3.14

--- Formula/obsidian-headless.rb
+++ Formula/obsidian-headless.rb
@@ -8,7 +8,7 @@ class ObsidianHeadless < Formula
   depends_on "node"
 
   on_linux do
-    depends_on "python@3.13" => :build
+    depends_on "python@3.14" => :build
     depends_on "gcc" => :build
   end
 
@@ -20,7 +20,7 @@ class ObsidianHeadless < Formula
   end
 
   def install
-    ENV["npm_config_python"] = Formula["python@3.13"].opt_bin/"python3" if OS.linux?
+    ENV["npm_config_python"] = Formula["python@3.14"].opt_bin/"python3" if OS.linux?
     system "npm", "install", *std_npm_args
     bin.install_symlink libexec/"bin/ob"
   end
