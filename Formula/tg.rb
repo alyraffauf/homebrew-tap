a210cb470d9b6d92ab5dd4dabd25a9c4677d39d8
Aly Raffauf <aly@aly.codes>
2026-07-12 15:12:00 -0400

    formula/tg: update to version 20260710 and add livecheck

--- Formula/tg.rb
+++ Formula/tg.rb
@@ -1,9 +1,17 @@
 class Tg < Formula
   desc "Command-line client for Tangled, the git forge built on atproto"
   homepage "https://github.com/alyraffauf/tg"
-  url "https://github.com/alyraffauf/tg/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "bf07ffee919da5c8e7dd49d7081dc835efb41df001a2b88817607aa597c54932"
+  url "https://github.com/alyraffauf/tg/archive/451de824d8ad65faa13bb36cef1f9243bb4234ed.tar.gz"
+  version "20260710"
+  sha256 "665243662045ebf449adf0f4113d7f8c810897f194a5bf1ce4062d6f7612bf88"
   license "GPL-3.0-or-later"
+
+  livecheck do
+    url "https://api.github.com/repos/alyraffauf/tg/commits/master"
+    strategy :json do |json|
+      json["commit"]["author"]["date"][0..9].tr("-", "")
+    end
+  end
 
   depends_on "go" => :build
 
@@ -12,6 +20,6 @@ class Tg < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/tg --version 2>&1", 0)
+    assert_match "Tangled", shell_output("#{bin}/tg --help")
   end
 end
