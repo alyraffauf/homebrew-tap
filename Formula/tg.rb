5294cd8c6ec64db8216d1dbaede0202d7b711f93
Aly Raffauf <aly@aly.codes>
2026-07-31 11:51:12 -0400

    tg: v0.4.0

--- Formula/tg.rb
+++ Formula/tg.rb
@@ -1,17 +1,9 @@
 class Tg < Formula
   desc "Command-line client for Tangled, the git forge built on atproto"
   homepage "https://github.com/alyraffauf/tg"
-  url "https://github.com/alyraffauf/tg/archive/451de824d8ad65faa13bb36cef1f9243bb4234ed.tar.gz"
-  version "20260710"
-  sha256 "665243662045ebf449adf0f4113d7f8c810897f194a5bf1ce4062d6f7612bf88"
+  url "https://github.com/alyraffauf/tg/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "bd9a051017805b930edf8ab56b9e696da82f02acc9eca294366544a8c39fb2cf"
   license "GPL-3.0-or-later"
-
-  livecheck do
-    url "https://api.github.com/repos/alyraffauf/tg/commits/master"
-    strategy :json do |json|
-      json["commit"]["author"]["date"][0..9].tr("-", "")
-    end
-  end
 
   depends_on "go" => :build
 
